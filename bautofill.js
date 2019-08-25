5cdf8bc0ec9fbbed0d7bdac394ca61bbeb2879e7
Benjamin Ehrlich <bensaysstuff@gmail.com>
2019-08-25 15:10:46 -0400

    Overhaul with major improvements.
    Removing jQuery dependency, increasing performance, readability, and new car smell.

--- bautofill.js
+++ bautofill.js
@@ -1,95 +1,67 @@
-$( document ).ready(function() {
-
+function bautofill_doDrop(el) {
 	
-	//Create lists underneath the text elements
-	if(document.getElementsByClassName('bautoCompleteInput').length > 0) {
-
-
-		var autoCompletes = document.getElementsByClassName('bautoCompleteInput');
-		
-		for(var v = 0; v < autoCompletes.length; v++) {
-		
-			var bautoCompleteList = document.createElement("ul");
-			bautoCompleteList.className = 'bautoCompleteList';
-			bautoCompleteList.id = autoCompletes[v].id + "-list";
-			
-			bautoCompleteList.style['margin-left'] = autoCompletes[v].offsetLeft-8+autoCompletes[v].scrollLeft;
-			bautoCompleteList.style['width'] = autoCompletes[v].clientWidth
-			
-			autoCompletes[v].insertAdjacentHTML("afterend",bautoCompleteList.outerHTML)
-		}
-	}
-
+	let items = [];
 	
-
-	menuHasFocus = false;
-	 
-	$(".bautoCompleteInput").keydown(function(e) { displayMenu(this,e); });
-	$(".bautoCompleteInput").keyup(function(e) { displayMenu(this,e); }); 
-	$(".bautoCompleteInput").blur(function(e) { closeTheMenu(this); } );
-	 
-
-	function closeTheMenu(element) {
-		if(menuHasFocus == false) { 
-			document.getElementById(element.id+"-list").innerHTML = ""; 
-			document.getElementById(element.id+"-list").style["visibility"] = "hidden"; 
-		}
-		
+	try {
+		items = JSON.parse(el.dataset.items);
 	}
-	 
-	function displayMenu(selectedTextBox,ev) {
-		
-		var thisDataSet = selectedTextBox.dataset['filler']; 
-		
-		try {
-			autoCompleteData = JSON.parse(thisDataSet); 
-		} catch(e) { autoCompleteData = [e]; }
-
-		//Exit the field if the user pressed enter
-		var keycode = (ev.keyCode ? ev.keyCode : ev.which);
-		if(keycode == '13'){ ev.preventDefault(); closeTheMenu(selectedTextBox); return true; }
-
-
-		var fieldValue = selectedTextBox.value;
-		var thisList = document.getElementById(selectedTextBox.id + "-list");
-		thisList.style["visibility"] = "visible";
-		
-		
-		thisList.innerHTML = "";
-		
-		for(var v = 0; v < autoCompleteData.length; v++) {
-		
-		
-			if(autoCompleteData[v].toLowerCase().indexOf(fieldValue.toLowerCase()) != -1 && fieldValue != "") {
-				
-				
-				var newNode = document.createElement("li");
-
-				newNode.name = autoCompleteData[v];
-				newNode.className = "bautoCompleteItem"; 
-				newNode.style['width'] = selectedTextBox.style['width'];
-
-				newNode.onclick = function(){
-					
-					selectedTextBox.focus(); 
-					selectedTextBox.value = this.name; 
-					
-					for(var v = 0; v < document.getElementsByClassName("bautoCompleteList").length; v++) {
-						document.getElementsByClassName("bautoCompleteList")[v].innerHTML = "";
-						document.getElementsByClassName("bautoCompleteList")[v].style['visibility'] = "hidden";
-					}
-				} 
-				
-				
-				newNode.onmouseover = function() { menuHasFocus = selectedTextBox; }
-				newNode.onmouseout = function() { menuHasFocus = false; }
-				
-				
-				var textNode = document.createTextNode(autoCompleteData[v]);
-				newNode.appendChild(textNode);
-				
-				thisList.appendChild(newNode);
-			}
-		}
+	catch(e) {
+		console.error("Malformed JSON, couldn't populate list.");
+		return false;
 	}
+	
+	let foundMatch = false; 
+	let oldDropdown = document.getElementById("bautofill_dropDown");
+	
+	if(oldDropdown !== null) { oldDropdown.remove(); }
+	
+	let dropDownArea = document.createElement("div");
+	dropDownArea.id = "bautofill_dropDown";
+	dropDownArea.style.position = "absolute";
+	dropDownArea.style.top = el.offsetTop + el.offsetHeight + "px";
+	dropDownArea.style.left = el.offsetLeft + "px";
+	dropDownArea.style.width = el.offsetWidth-2 + "px"; 
+	
+	let dropDownList = document.createElement("ul");
+	dropDownList.classList.add("bautofill_dropDownList");
+	dropDownArea.appendChild(dropDownList);
+
+	items.forEach(item => {
+	
+		if(item.indexOf(el.value) !== -1 && item !== el.value) {
+		
+			foundMatch = true;
+			
+			let listItem = document.createElement("li");
+			listItem.innerHTML = item; 
+			listItem.classList.add("bautofill_listItem");
+			listItem.addEventListener("mousedown", ev => {
+			
+				el.value = item;
+			});
+			
+			dropDownArea.getElementsByTagName("ul")[0].appendChild(listItem);
+		}
+	}); 
+	
+	if(foundMatch) {
+	
+		document.body.appendChild(dropDownArea);
+	}
+}
+
+Array.from(document.getElementsByClassName('bautofill')).forEach(el => {
+	
+	
+	el.addEventListener("keydown", ev => { bautofill_doDrop(el); });
+	el.addEventListener("keyup", ev => { bautofill_doDrop(el); }); 
+	el.addEventListener("focus", ev => { bautofill_doDrop(el); });
+	
+	el.addEventListener("blur", ev => {
+		
+		let oldDropdown = document.getElementById("bautofill_dropDown");
+		if(oldDropdown !== null) { oldDropdown.remove(); }
+	});
+	
+	
 });
